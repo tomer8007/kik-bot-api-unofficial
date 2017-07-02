0140386f0a482ac4a4a822bbb32392b04d247d00
Tomer <tomer8008@gmail.com>
2017-07-02 03:27:25 +0300

    Add files via upload

---
+++ examples/echo_bot.py
@@ -0,0 +1,66 @@
+import sys
+from KikClient import KikClient
+from Utilities import Utilities
+
+import time
+
+
+def main():
+    username, password = "emilyajar", "12345678"
+    kik_client = KikClient(username, password)
+
+    print("[+] Listening for incoming events.")
+
+    # main events loop
+    while True:
+
+        info = kik_client.get_next_event()
+        if "type" not in info:
+            continue
+
+        if info["type"] == "message_read":
+            print("[+] Human has read the message (user "+info["from"]+", message id: "+info["message_id"]+")")
+
+        elif info["type"] == "is_typing":
+            if info["is_typing"]:
+                print("[+] Human is typing (user "+info["from"]+")")
+            else:
+                print("[+] Human is not typing (user "+info["from"]+")")
+
+        elif info["type"] == "message":
+            partner = info["from"]
+            print("[+] Human says: \"" + info["body"] + "\" (user "+partner+")")
+            is_known_user = False
+            for jid_info in chat_partners:
+                jid = jid_info["node"] + "@talk.kik.com"
+                if jid == partner:
+                    is_known_user = True
+                    break
+
+            if not is_known_user:
+                print("[+] This is a message from an unknown user.")
+                new_friend = kik_client.add_friend(partner)
+                chat_partners.append(new_friend)
+
+            kik_client.send_read_confirmation(partner, info["message_id"])
+            replay = "You said '"+info["body"] + "'!"
+            kik_client.send_is_typing(partner, "true")
+            time.sleep(0.2 * len(replay))
+            kik_client.send_is_typing(partner, "false")
+            kik_client.send_message(partner, replay)
+
+        elif info["type"] == "end":
+            print("[!] Server ended communication.")
+            break
+
+    print("[+] Done!")
+    kik_client.close()
+
+
+if __name__ == '__main__':
+    if sys.version_info[0] < 3:
+        raise Exception("Must be using Python 3!!")
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("[!] User stopped execution.")

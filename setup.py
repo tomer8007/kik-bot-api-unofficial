7068432dd487155420e356ba699b35c64267230c
Tomer <tomer8008@gmail.com>
2017-11-16 15:48:58 +0200

    First version 0.1

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 setup(
     name='kik_unofficial',
 
-    version='0.0.1',
+    version='0.1',
 
     description='Python API for writing unoffical kik bots that act like humans',
     long_description=long_description,

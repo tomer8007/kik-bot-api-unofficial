ebd8d8b023e9e20727ed6038e51c358e57aaec1f
Tomer <tomer8008@gmail.com>
2018-09-14 18:11:58 +0300

    Minor comments fix in device_configuration.py

--- kik_unofficial/device_configuration.py
+++ kik_unofficial/device_configuration.py
@@ -2,8 +2,7 @@
 Here we put all the device configuration that we emulate.
 """
 
-# possible kik versions to emulate.
-# note that a kik version that's not updated might cause a captcha on login
+# possible kik versions to emulate
 kik_version_11_info = {"kik_version": "11.1.1.12218", "classes_dex_sha1_digest": "aCDhFLsmALSyhwi007tvowZkUd0="}
 kik_version_13_info = {"kik_version": "13.4.0.9614", "classes_dex_sha1_digest": "ETo70PFW30/jeFMKKY+CNanX2Fg="}
 kik_version_14_info = {"kik_version": "14.0.0.11130",  "classes_dex_sha1_digest": "9nPRnohIOTbby7wU1+IVDqDmQiQ="}
@@ -11,6 +10,6 @@ kik_version_14_5_info = {"kik_version": "14.5.0.13136", "classes_dex_sha1_digest
 
 
 device_id = "62030843678b7376a707ca3d11e87837"  # random 16 bytes. you can set it to anything, but stick with it
-kik_version_info = kik_version_14_5_info          # upgrading to later versions might trigger SafetyNet problems
+kik_version_info = kik_version_14_5_info        # a kik version that's not updated might cause a captcha on login
 android_id = "849d4ffb0c020de7"                 # random 8 bytes. you can set it to anything, but stick with it
 

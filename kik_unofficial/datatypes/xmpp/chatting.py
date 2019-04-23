0170e4bf961750444ee61542d6ba3bd431500252
Jaap Heijligers <jaap@heijligers.me>
2019-04-23 16:31:06 +0200

    Fix pm image bug

--- kik_unofficial/datatypes/xmpp/chatting.py
+++ kik_unofficial/datatypes/xmpp/chatting.py
@@ -287,7 +287,7 @@ class IncomingImageMessage(XMPPResponse):
         self.status = data.status.text if data.status else None
         self.from_jid = data['from']
         self.to_jid = data['to']
-        self.group_jid = data.g['jid']
+        self.group_jid = data.g['jid'] if data.g else None
 
 
 class IncomingGroupSticker(XMPPResponse):

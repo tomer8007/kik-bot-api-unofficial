592038fb7b795371eb9f5265ebb6225dc82a5d32
Yahya Al-Shamali <yaalshama@gmail.com>
2023-08-11 22:49:41 -0600

    Update echo_bot.py

--- examples/echo_bot.py
+++ examples/echo_bot.py
@@ -4,7 +4,6 @@ A Kik bot that just logs every event that it gets (new message, message read, et
 and echos back whatever chat messages it receives.
 """
 
-import argparse
 import yaml
 import os
 
@@ -27,7 +26,6 @@ def main():
     # load the bot's credentials from creds.yaml
     with open(creds_file) as f:
         creds = yaml.safe_load(f)
-        print(creds)
 
     # create the bot
     bot = EchoBot(creds)
@@ -41,12 +39,12 @@ class EchoBot(KikClientCallback):
         # optional parameters
         device_id = creds['device_id']
         android_id = creds['android_id']
-        node = creds.get('node') # If know it, set it to None
-        
+        node = creds.get('node') # If you don't know it, set it to None
         
         self.client = KikClient(self, username, password, node, device_id=device_id, android_id=android_id, logger=True)
         self.client.wait_for_messages()
 
+    # Initialization and Authentication
     def on_authenticated(self):
         print("Now I'm Authenticated, let's request roster")
         self.client.request_roster()
@@ -54,19 +52,24 @@ class EchoBot(KikClientCallback):
     def on_login_ended(self, response: LoginResponse):
         print(f"Full name: {response.first_name} {response.last_name}")
 
+    # Chat and Group Messages
     def on_chat_message_received(self, chat_message: chatting.IncomingChatMessage):
         print(f"[+] '{chat_message.from_jid}' says: {chat_message.body}")
         print("[+] Replaying.")
         self.client.send_chat_message(chat_message.from_jid, "You said \"" + chat_message.body + "\"!")
 
+    def on_group_message_received(self, chat_message: chatting.IncomingGroupChatMessage):
+        print(f"[+] '{chat_message.from_jid}' from group ID {chat_message.group_jid} says: {chat_message.body}")
+    
+    def on_image_received(self, image_message: chatting.IncomingImageMessage):
+        print(f"[+] Image message was received from {image_message.from_jid}")
+        
+    # Events and Statuses
     def on_message_delivered(self, response: chatting.IncomingMessageDeliveredEvent):
         print(f"[+] Chat message with ID {response.message_id} is delivered.")
 
     def on_message_read(self, response: chatting.IncomingMessageReadEvent):
         print(f"[+] Human has read the message with ID {response.message_id}.")
-
-    def on_group_message_received(self, chat_message: chatting.IncomingGroupChatMessage):
-        print(f"[+] '{chat_message.from_jid}' from group ID {chat_message.group_jid} says: {chat_message.body}")
 
     def on_is_typing_event_received(self, response: chatting.IncomingIsTypingEvent):
         print(f'[+] {response.from_jid} is now {"" if response.is_typing else "not "}typing.')
@@ -77,12 +80,6 @@ class EchoBot(KikClientCallback):
     def on_roster_received(self, response: FetchRosterResponse):
         print("[+] Chat partners:\n" + '\n'.join([str(member) for member in response.peers]))
 
-    def on_friend_attribution(self, response: chatting.IncomingFriendAttribution):
-        print(f"[+] Friend attribution request from {response.referrer_jid}")
-
-    def on_image_received(self, image_message: chatting.IncomingImageMessage):
-        print(f"[+] Image message was received from {image_message.from_jid}")
-    
     def on_peer_info_received(self, response: PeersInfoResponse):
         print(f"[+] Peer info: {str(response.users)}")
 
@@ -94,15 +91,18 @@ class EchoBot(KikClientCallback):
 
     def on_status_message_received(self, response: chatting.IncomingStatusResponse):
         print(f"[+] Status message from {response.from_jid}: {response.status}")
+        
+    def on_friend_attribution(self, response: chatting.IncomingFriendAttribution):
+        print(f"[+] Friend attribution request from {response.referrer_jid}")
 
+    # Account Management
     def on_username_uniqueness_received(self, response: UsernameUniquenessResponse):
         print(f"Is {response.username} a unique username? {response.unique}")
 
     def on_sign_up_ended(self, response: RegisterResponse):
         print(f"[+] Registered as {response.kik_node}")
 
-    # Error handling
-
+    # Error Handling
     def on_connection_failed(self, response: ConnectionFailedResponse):
         print(f"[-] Connection failed: {response.message}")
 

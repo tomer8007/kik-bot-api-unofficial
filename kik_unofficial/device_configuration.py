b502386c51389d53d4c3e93adf36acc158d3bf5c
Blue Mods <53561555+bluemods@users.noreply.github.com>
2023-12-05 14:31:47 -0500

    Add new kik version info (Kik now uses sha256 digest)

--- kik_unofficial/device_configuration.py
+++ kik_unofficial/device_configuration.py
@@ -2,8 +2,6 @@
 Here we put all the device configuration that we emulate.
 """
 
-from kik_unofficial.configuration import env
-
 # possible kik versions to emulate
 kik_version_11_info = {"kik_version": "11.1.1.12218", "classes_dex_sha1_digest": "aCDhFLsmALSyhwi007tvowZkUd0="}
 kik_version_13_info = {"kik_version": "13.4.0.9614", "classes_dex_sha1_digest": "ETo70PFW30/jeFMKKY+CNanX2Fg="}
@@ -12,6 +10,6 @@ kik_version_14_5_info = {"kik_version": "14.5.0.13136", "classes_dex_sha1_digest
 kik_version_15_21_info = {'kik_version': '15.21.0.22201', 'classes_dex_sha1_digest': 'MbZ+Zbjaz5uFXKFDM88CwFh7DAg='}
 kik_version_15_49_info = {'kik_version': '15.49.0.27501', 'classes_dex_sha1_digest': '5o61frOsakJJ2iCYafCoKHtyu7w='}
 kik_version_15_57_info = {'kik_version': '15.57.2.29235', 'classes_dex_sha1_digest': 'hA77Y2jUTVbpHRB9LosnnunQ1PY='}
+kik_version_15_60_info = {'kik_version': '15.60.1.29587', 'classes_dex_sha1_digest': 'FXxvP2QjSj+sXp+G1MqDdxz8Z51YjtqzFOQ7wlex0VM='}
 
-kik_version_info = kik_version_15_57_info        # a kik version that's not updated will cause a captcha on login
-
+kik_version_info = kik_version_15_60_info  # a kik version that's not updated will cause a captcha on login

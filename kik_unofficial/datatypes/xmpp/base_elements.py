c15df46c426a39b08273efec4903c79faa75842c
Blue Mods <53561555+bluemods@users.noreply.github.com>
2023-12-05 13:12:35 -0500

    Document base elements

--- kik_unofficial/datatypes/xmpp/base_elements.py
+++ kik_unofficial/datatypes/xmpp/base_elements.py
@@ -13,6 +13,11 @@ from kik_unofficial.utilities.kik_server_clock import KikServerClock
 
 
 class XMPPElement:
+    """
+    Represents an outgoing stanza of any kind.
+
+    Subclasses must override `serialize`.
+    """
     def __init__(self):
         self.message_id = CryptographicUtils.make_kik_uuid()
 
@@ -21,12 +26,21 @@ class XMPPElement:
 
 
 class XMPPOutgoingMessageElement(XMPPElement):
+    """
+    Represents an outgoing message of any kind to a user.
+
+    Subclasses must override `serialize_message` and add
+    the child elements necessary to build the message stanza for sending.
+    """
     def __init__(self, peer_jid: str):
         super().__init__()
         self.peer_jid = peer_jid
         self.is_group = jid_utilities.is_group_jid(peer_jid)
         self.timestamp = str(KikServerClock.get_server_time())
         self.message_type = "groupchat" if self.is_group else "chat"
+
+    def serialize_message(self, message: Element) -> None:
+        raise NotImplementedError
 
     @final
     def serialize(self) -> Element:
@@ -41,11 +55,15 @@ class XMPPOutgoingMessageElement(XMPPElement):
         self.serialize_message(message)
         return message
 
-    def serialize_message(self, message: Element) -> None:
-        raise NotImplementedError
-
     @final
     def add_kik_element(self, message: Element, push: bool = True, qos: bool = True) -> None:
+        """
+        Adds a kik child element. Exactly one of these must be present in every outgoing message.
+
+        :param message: the message parameter from serialize_message()
+        :param push: if True, the recipient will be sent a push notification by Kik.
+        :param qos: if True, the message will be placed in the QoS pool (the client can receive it while disconnected)
+        """
         kik = etree.SubElement(message, 'kik')
         kik.set('push', 'true' if push else 'false')
         kik.set('qos', 'true' if qos else 'false')
@@ -53,6 +71,13 @@ class XMPPOutgoingMessageElement(XMPPElement):
 
     @final
     def add_request_element(self, message: Element, request_delivered: bool = True, request_read: bool = False) -> None:
+        """
+        Adds a request element. Request elements are indicators to the receiving client for sending receipts.
+
+        :param message: the message parameter from serialize_message()
+        :param request_delivered: if True, the receiving client sends a delivered back when the message is received
+        :param request_read: if True, the receiving client sends a read receipt when the message is opened or read
+        """
         if not request_read and not request_delivered:
             return
         request = etree.SubElement(message, 'request')
@@ -62,10 +87,25 @@ class XMPPOutgoingMessageElement(XMPPElement):
 
     @final
     def add_empty_element(self, message: Element, name: str) -> None:
+        """
+        Adds an empty element.
+
+        For example:
+        self.add_empty_element(message, 'foo')
+        becomes '<foo></foo>' when serialized.
+        """
         etree.SubElement(message, name).text = ''
 
 
 class XMPPOutgoingContentMessageElement(XMPPOutgoingMessageElement):
+    """
+    Represents an outgoing content message.
+
+    Content messages can be images, gifs, videos, stickers, or custom cards.
+
+    Subclasses must override `serialize_content` and add
+    the elements necessary to build the content for sending.
+    """
     def __init__(self, peer_jid: str, app_id: str):
         super().__init__(peer_jid)
         self.content_id = str(uuid.uuid4())  # content ids arent cryptographic
@@ -99,22 +139,50 @@ class XMPPOutgoingContentMessageElement(XMPPOutgoingMessageElement):
 
     @final
     def set_allow_forward(self, allow_forward: bool) -> None:
+        """
+        Sets the forwarding flag for content.
+
+        :param allow_forward: True if the client is allowed to forward the content
+        """
         self.add_string('allow-forward', 'true' if allow_forward else 'false')
 
     @final
     def set_allow_save(self, allow_save: bool) -> None:
+        """
+        Sets the allow save flag for content.
+
+        :param allow_save: True if the client is allowed to save the content
+        """
         self.add_string('disallow-save', 'false' if allow_save else 'true')
 
     @final
     def set_video_autoplay(self, auto_play: bool) -> None:
+        """
+        Sets the auto play flag for content.
+        This has no effect if not a video / GIF.
+
+        :param auto_play: True if the client should auto-play the content
+        """
         self.add_string('video-should-autoplay', 'true' if auto_play else 'false')
 
     @final
     def set_video_loop(self, loop: bool) -> None:
+        """
+        Sets the video loop flag for content.
+        This has no effect if not a video / GIF.
+
+        :param loop: True if the client should play the video on a loop
+        """
         self.add_string('video-should-loop', 'false' if loop else 'true')
 
     @final
     def set_video_muted(self, muted: bool) -> None:
+        """
+        Sets the muted flag for content.
+        This has no effect if not a video / GIF.
+
+        :param muted: True if the client should mute the video when playing it
+        """
         self.add_string('video-should-be-muted', 'true' if muted else 'false')
 
     @final
@@ -122,25 +190,64 @@ class XMPPOutgoingContentMessageElement(XMPPOutgoingMessageElement):
         etree.SubElement(self._content_strings, name).text = value
 
     @final
-    def add_image(self, name: str, image_base64: str) -> None:
+    def add_image(self, name: str, image_bytes: bytes) -> None:
+        """
+        Adds an image to the content message.
+
+        'icon' should be in PNG format and be a perfect square (64x64px or less)
+        'preview' should be in JPG format.
+        'png-preview' should be in PNG format.
+
+        :param name: the image name. Must be one of 'icon' 'preview' 'png-preview'
+        :param image_bytes: the image bytes
+        """
         if name not in ('icon', 'preview', 'png-preview'):
             raise ValueError(f'invalid image name {name}, must be icon, preview, png-preview')
-        etree.SubElement(self._content_images, name).text = image_base64
+        etree.SubElement(self._content_images, name).text = base64.b64encode(image_bytes)
 
     @final
     def add_hash(self, name: str, value: str) -> None:
+        """
+        Adds a hash to the content message.
+
+        Hashes should only be used for image messages.
+
+        :param name: the hash name. Must be one of 'sha1-original' 'sha1-scaled' 'blockhash-scaled'
+        :param value: the hash value
+        """
         if name not in ('sha1-original', 'sha1-scaled', 'blockhash-scaled'):
             raise ValueError(f'invalid hash name {name}, must be sha1-original, preview, blockhash-scaled')
         etree.SubElement(self._content_hashes, name).text = value
 
     @final
     def add_extra(self, key: str, value: str) -> None:
+        """
+        Adds an extra to the content message.
+
+        Extras are key value pairs forwarded to a browser when the message is clicked and contains a link.
+
+        :param key: the extra key
+        :param value: the extra value
+        """
         item = etree.SubElement(self._content_extras, 'item')
         etree.SubElement(item, 'key').name = key
         etree.SubElement(item, 'val').name = value
 
     @final
     def add_uri(self, url: str, platform: str = None, type: str = None, file_content_type: str = None, priority: str = None) -> None:
+        """
+        Adds a uri to the content message.
+
+        Uris are URLs to content.
+        For GIFs, these are used to play back the GIF.
+        For other content types, the link is opened in the browser when the content is tapped.
+
+        :param url: the URL itself
+        :param platform: the platform of the URI
+        :param type: the type of the URI
+        :param file_content_type: the file content type of the URI
+        :param priority: the priority of the URI. This should be a positive integer.
+        """
         uri = etree.SubElement(self._content_uris, 'uri')
         uri.text = url
         if platform:
@@ -154,6 +261,9 @@ class XMPPOutgoingContentMessageElement(XMPPOutgoingMessageElement):
 
 
 class XMPPOutgoingIsTypingMessageElement(XMPPOutgoingMessageElement):
+    """
+    An outgoing is typing message to a group
+    """
     def __init__(self, peer_jid: str, is_typing: bool):
         super().__init__(peer_jid)
         self.message_type = 'is-typing'  # IsTyping messages always use this type, even if it's to a group
@@ -161,6 +271,11 @@ class XMPPOutgoingIsTypingMessageElement(XMPPOutgoingMessageElement):
 
 
 class XMPPResponse:
+    """
+    This is an incoming stanza from Kik.
+
+    When a message stanza is encountered, this will parse the basic attributes of the message.
+    """
     def __init__(self, data: BeautifulSoup):
         self.message_id = data['id']
         self.raw_element = data
@@ -287,6 +402,13 @@ class XMPPContentResponse(XMPPResponse):
                 raise ValueError(f"invalid file-url (expected https://platform.kik.com, received {self.file_url})")
 
     class ContentUri:
+        """
+        A content URI.
+
+        Uris are URLs to content.
+        For GIFs, these are used to play back the GIF.
+        For other content types, the link is opened in the browser when the content is tapped.
+        """
         def __init__(self, uri: BeautifulSoup):
             self.platform = uri['platform']
             self.type = uri['type']
@@ -296,6 +418,9 @@ class XMPPContentResponse(XMPPResponse):
 
 
 class XMPPReceiptResponse(XMPPResponse):
+    """
+    An incoming receipt received from another user.
+    """
     def __init__(self, data: BeautifulSoup):
         super().__init__(data)
         receipt = data.find('receipt', recursive=False)
